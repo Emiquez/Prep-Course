be77ca4c9ac2c7c71d341432aaca385d46bace4c
Emiliano <saranoemiliano@gmail.com>
2022-03-13 10:03:15 -0300

    03-js-II homework done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -160,23 +160,35 @@ function esVerdadero(valor){
 
 }
 
-function tablaDelSeis(tabla){
+function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
+  const tabla = [];
+  for (let i = 0; i <= 10; i++) {
+    tabla.push(i*6);
+  } return tabla;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero / 100 >= 1 && numero / 1000 < 1) {
+    return true;
+  } return false
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-}
+  let i = 0
+  do {
+    i = i + 1;
+    numero = numero + 5;
+  } while (i < 8);
+    return numero;
+ }
 
 
 // No modificar nada debajo de esta línea
